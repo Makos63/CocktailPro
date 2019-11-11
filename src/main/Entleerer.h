759054c8fcd860a9994e5ebe4db4124415f4a1c1
Kehr,Christian <christian.kehr@stud.h-da.de>
2019-11-11 21:57:59 +0100

    commented my files

--- src/main/Entleerer.h
+++ src/main/Entleerer.h
@@ -1,4 +1,12 @@
 //@(#) Entleerer.h
+/**
+*	@class Entleerer
+*
+* 	@brief Class to empty the mixer
+*
+* 	The Class checks, if the right weight is reached.
+* 	If so, the mixer prints a message and empties with the given speed.   
+*/
 
 #ifndef ENTLEERER_H_H
 #define ENTLEERER_H_H
@@ -11,17 +19,53 @@
 
 class Entleerer : public InternalDevice, public Observer {
 public:
+
+/**
+* 	@brief Default constructor.
+*
+* 	@param Gets Parmeter: weight (g)
+*	@param Gets Parmeter: time (ze) 
+*	@param Gets Parmeter: scale (wg)
+*
+* 	@return no return
+*/
     Entleerer(float g, int ze, Waage * wg);
     ~Entleerer();
+	
+	/**
+	* 	@brief Checks, if weight was reached.
+	*
+	* 	@param no parameters
+	*
+	* 
+	*/
     void update();
 
+	/**
+	*	@brief Empties the mixer.
+	*
+	*	@param takes the float "value"
+	*
+	*	Prints the weight and that the empty-process starts.
+	*	Outputs cocktail as long as "doinIt" is true and gets the value of outputted fluid.
+	*/
     void doIt(float value);
 
 private:
-    float grammProZeit;
 
+	/**
+	*	Variable wich contains the output value in g/t
+	*/
+    float grammProZeit;
+	
+	/**
+	*  Object from the Type "myWaage"
+	*/
     Waage * myWaage;
 
+	/**
+	*	Amount to output 
+	*/
     float value;
 
 };
--- src/main/MischbaresRezeptbuch.h
+++ src/main/MischbaresRezeptbuch.h
@@ -1,4 +1,12 @@
 //@(#) MischbaresRezeptbuch.h
+/**
+*	@class MischbaresRezeptbuch
+*
+* 	@brief outputs the available recipes
+*
+* 	Prints all available recipes and has the ability to check, 
+* 	which recipes are possible and if not, deletes them from the current vector
+*/
 
 #ifndef MISCHBARESREZEPTBUCH_H_H
 #define MISCHBARESREZEPTBUCH_H_H
@@ -19,16 +27,63 @@
  */
 class MischbaresRezeptbuch : public RecipeBook {
 public:
+
+	/**
+	*	@brief Calls standart constructor
+	*	
+	*	@param object of "Vorhandene Zutaten"
+	*
+	*	@return none
+	*
+	*	prints "all available recipes before filtering"
+	*	calls browse, to print the recipes
+	*/
     MischbaresRezeptbuch(VorhandeneZutaten * ze);
+	
+	/**
+	*	@brief Prints Recipes.
+	*	
+	*	@param none.
+	*
+	*	@return none.
+	*
+	*	prints number of recipes.
+	*	then it  iterates through the vector "rezepte" and calls the browse for the object in the vector
+	*/
     void browse();
 
 private:
+	/**
+	*	Object of the type "myZutatenverwalter" to check, which ingredients are available
+	*/
     VorhandeneZutaten * myZutatenVerwalter;
 
+	/**
+	*	vector of Recipe pointer, to store all Recipes
+	*/
     std::vector<Recipe *> rezepte;
 
+	/**
+	*	@brief	deletes non available recipes
+	*
+	*	@param none
+	*
+	*	@return none
+	*
+	*	iterates through all recipes and checks, if all ingredients are available.
+	*	if not, it deletes it, to reduce size of the vector
+	*/
     void loeschen();
 
+	/**
+	*	@brief transferes the "VorhandeneZutaten" to myZutatenVerwalter
+	*
+	*	@param pointer to VorhandeneZutaten
+	*
+	*	@return none;
+	*
+	*	sets myZutatenVerwalter to VorhandeneZutaten
+	*/
     void setZutatenVerwalter(VorhandeneZutaten * ze);
 
 };
