e2e5a3dfa56bf7c8ee45ad68f891aa71c92bc0ae
Andari,Jelita Ratu <Jelita.R.Andari@stud.h-da.de>
2019-11-12 10:18:43 +0100

    add comment

--- src/main/Schuettler.h
+++ src/main/Schuettler.h
@@ -3,12 +3,29 @@
 #ifndef SCHUETTLER_H_H
 #define SCHUETTLER_H_H
 
+/**
+* @class Schuettler
+*
+* @brief is derived class of base class InternalDevice
+*
+* to control the Shaker using the Timer
+*/
+
 #include "InternalDevice.h"
 
 class Schuettler : public InternalDevice {
 public:
+
+/**
+* @brief constructor of class Schuettler
+*/
     Schuettler();
 
+/**
+* @brief sets the timer of shaker
+* @param time is a float and it depends on the timer of InternalDevice
+*
+*/
     void doIt(float time);
 
 private:
--- src/main/Stampfer.h
+++ src/main/Stampfer.h
@@ -3,11 +3,29 @@
 #ifndef STAMPFER_H_H
 #define STAMPFER_H_H
 
+/**
+* @class Stampfer
+*
+* @brief is a derived class of base class InternalDevice
+*
+* controls the masher
+*/
+
 #include "InternalDevice.h"
 
 class Stampfer : public InternalDevice {
 public:
+
+/**
+* @brief constructor for class Stampfer
+* has no parameters
+*/
     Stampfer();
+
+/**
+* @brief sets the timer of masher
+* @param time is a float and it depends on the timer of InternalDevice
+*/
 
     void doIt(float time);
 
