7f3e67abaa5d9eb7e5a8ddefbea0385d2b6caf64
Kehr,Christian <christian.kehr@stud.h-da.de>
2019-11-17 19:19:06 +0100

    added comment in Entleerer .h

--- src/main/Entleerer.h
+++ src/main/Entleerer.h
@@ -40,7 +40,7 @@ public:
 	/**
 	*	@brief Empties the mixer.
 	*
-	*	@param value represents the amount that needs to be given out
+	*	@param valueInput represents the amount that needs to be given out
 	*
 	*	Prints the weight and that the empty-process starts.
 	*	Outputs cocktail as long as "doinIt" is true and gets the value of outputted fluid.
